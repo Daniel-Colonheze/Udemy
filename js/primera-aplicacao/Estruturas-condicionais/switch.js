b296cb53b51ca1d3bd7401713efb872d67b38282
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-02-24 18:06:55 -0300

    switch

---
+++ js/primera-aplicacao/Estruturas-condicionais/switch.js
@@ -0,0 +1,15 @@
+let opcao = 1;
+switch(opcao){
+    case 1: 
+        console.log("Saldo da conta");
+        break;
+    case 2:
+        console.log("Cartão de Credito");
+        break
+    default:
+        console.log("Opcão invalida");
+}
+
+//Ternario
+let idade = 18
+idade >= 18 ? "Maior idade" : "Menor idade";
