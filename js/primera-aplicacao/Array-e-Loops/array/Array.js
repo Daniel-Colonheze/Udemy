0cb011a0f6219676045406aa5321ce68e7669f63
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-02-25 18:08:35 -0300

    Mais aprendizado legal

---
+++ js/primera-aplicacao/Array-e-Loops/array/Array.js
@@ -0,0 +1,4 @@
+let nomes = ["Daniel", "Leticia", "Bruno"]
+nomes.push("Fernando") //adicionar algo no array
+delete nomes[0]// deletar um item especifico
+console.log(nomes[0])
---
+++ js/primera-aplicacao/Array-e-Loops/contatenacao-e-templateString/exemplo.js
@@ -0,0 +1,8 @@
+const nome = "Daniel";
+const numero = 2
+
+//console.log("Bem vindo" + " " + nome) concatenacao simples
+
+const texto = `Bem vindo ${nome} voce tem ${numero} reais` //concatenacao com template string
+
+console.log(texto)
