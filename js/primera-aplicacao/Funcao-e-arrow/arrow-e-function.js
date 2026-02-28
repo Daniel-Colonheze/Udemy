82fde305c05e44954ffd5aaf7eb271b77214b8b5
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-02-28 19:49:37 -0300

    conteudo de arrow function finalizado

--- js/primera-aplicacao/Funcao-e-arrow/arrow-e-function.js
+++ js/primera-aplicacao/Funcao-e-arrow/arrow-e-function.js
@@ -8,3 +8,12 @@ somar(1,2)
 const somar = (a,b) => {
     console.log(a+b)
 }
+somar(1,2)
+
+//funcao arrow (retorno implicito )
+const somar = (a,b) => console.log(a+b)
+const exibir = n => console.log(n)
+const somar = (a,b) => a+b
+exibir("teste");
+let resultado = somar(1,2)
+console.log(resultado)
