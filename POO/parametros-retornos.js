496fb17b1878262aff8947c3c00b3ad510c0889a
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-03-05 18:38:03 -0300

    teste

--- POO/parametros-retornos.js
+++ POO/parametros-retornos.js
@@ -2,6 +2,7 @@ class Usuario {
     constructor(){
         this.email = ""
         this.senha = ""
+        this.subtotal = 0
     }
 
     logar (){
@@ -16,11 +17,31 @@ class Usuario {
             return "senha invalida"
         }
     }
+    calcularCupom( cupom ){
+        
+        let desconto = 0
+        if(cupom == "DESC20"){
+            desconto = 20
+        }else if( "FESTA10"){
+            desconto = 10
+        }else{
+            console.log("Cupom invalido")
+        }
+
+        return this.subtotal - desconto
+
+    }
 }
 
 const usuario = new Usuario()
+//autenticacao
 usuario.email = "daniel@gmail.com"
 usuario.senha = "1234"
+//desconto
+usuario.subtotal = 279
+
 
 let mensagem = usuario.logar()
+let verCupom =usuario.calcularCupom( "FESTA10")
+console.log(verCupom)
 console.log(mensagem)
