3f2c2a2182ab1cc7aa3f499b8c6bb91865e96e78
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-03-10 17:43:32 -0300

    finalizado heranca completa

--- heraca/heranca.js
+++ heraca/heranca.js
@@ -1,15 +1,13 @@
 // Herança - Reutilização e manutenção
 // Classe: Cao Passaro
 class Animal {//superclasse - pai
-  constructor(){
-    this.cor = ""
-    this.tamanho = 0
-    this.peso = 0
+  constructor(cor, tamanho, peso){
+    this.cor = cor
+    this.tamanho = tamanho
+    this.peso = peso
   }
   correr(){
-    console.log("correr")
-    console.log("como")
-    console.log("um")
+    console.log("correr como um")
     //30 linhas
   }
   dormir(){
@@ -18,9 +16,9 @@ class Animal {//superclasse - pai
 }
 
 class Cao extends Animal {//subclase - filha
-  constructor(){
-    super()
-    this.tamanhoOrelha = 0
+  constructor(cor, tamanho, peso, tamanhoOrelha){
+    super(cor, tamanho, peso)
+    this.tamanhoOrelha = tamanhoOrelha
   }
   correr(){
     super.correr()
@@ -32,29 +30,40 @@ class Cao extends Animal {//subclase - filha
 }
 
 class Passaro extends Animal {//subclase - filha
+  constructor(cor, tamanho, peso){
+    super(cor, tamanho, peso)
+  }
+  correr(){
+    super.correr()
+    console.log("passaro")
+  }
   voar(){
     console.log("voar")
   }
 }
 
 class Papagaio extends Passaro { //subsubclasse - filha da filha
+   constructor(cor, tamanho, peso, falar){
+    super(cor, tamanho, peso);
+    this.sabeFalar = falar
+   }
   falar(){
     console.log("falar");
   }
 }
 
 // Instancia
-const cao = new Cao()
-const passaro = new Passaro()
-const papagaio = new Papagaio()
+const cao = new Cao("Caramelo", 20, 40, 2)
+const passaro = new Passaro("Azul", 7, 2)
+const papagaio = new Papagaio("Marrom", 3, 4.9, true)
+const animal = new Animal("preto", "30", "95")
 
-//cao.correr()
-passaro.correr()
-passaro.voar()
-papagaio.falar()
+//console.log(animal.peso)
 //passaro.cor = "Amarelo"
 //console.log( passaro.cor )
-
+console.log(cao)
+console.log(passaro)
+console.log(papagaio)
 /*
 cao.correr()
 cao.latir()
