52e0c7288319b2cc72150bf67f80287f39bd649b
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-03-24 14:29:55 -0300

    comitando

---
+++ js/primera-aplicacao/Objetos-literais/script.js
@@ -0,0 +1,16 @@
+// cadastrando os produtos
+
+let nome = document.getElementById();
+let preco = 1200;
+
+const produto ={
+    nome,
+    preco,
+    exibirProduto(){
+        console.log(`${this.nome}, ${this.preco}`)
+    }
+}
+
+
+
+produto.exibirProduto();
