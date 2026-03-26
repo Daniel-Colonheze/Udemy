b76af0b4447d1d8cd355da4c83a325fd354c3305
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-03-26 17:42:34 -0300

    Organizando melhor as coisas

---
+++ js/DOM/selecionando-elementos/elementos-individuais.js
@@ -0,0 +1,7 @@
+const obj = document.getElementById('destaque') // pega pelo id
+const obj2 = document.querySelector('li.primeiro') // pega uma classe especifica
+
+function executar(argumento){
+    const obj2 = document.querySelector('li.primeiro')
+    obj2.innerHTML = "teste"
+}
